07f7a00561e4b0f20fadecb57242df426960b4c2
Teo344 <gabriel2004mat@gmail.com>
2024-12-13 12:51:07 -0500

    taller de javascript 13-12-2024

---
+++ Taller JavaScript 13-12-2024/js/First.js
@@ -0,0 +1,25 @@
+var nombre= "Mateo";
+var apellido="Criollo";
+var edad;
+edad=20;
+console.log("Mi nombre es " + nombre + " " + apellido);
+console.log("Mi edad es "+edad);
+console.log("Mi nombre es " + nombre + " " + apellido + " y tengo "+edad);
+
+let x=10;
+let y=20;
+let z=x+y;
+console.log(z);
+
+//var: declara una variable global
+//let: declara una variable local o tambien una variable de bloque
+//const declara una variable constante y que no cambia su valor
+
+//if marca un bloque de instrucciones
+//switch marca un bloque de instrucciones si una condicion se cumple
+//for maraca un bloque de instrucciones que se ejecuta varias veces
+//while mara un bloque de instrucciones que se eecutan varias veces
+//function marca un bloque de instrucciones que se ejecuta varia veces y se declara
+//try se ejecuta cuando no se produce un error
+//catch se ejecuta cuando se produce un error
+//return se ejecuta cuando una condicion se cumple, ademas de devolver un valor
